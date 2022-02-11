1e295a116ec857fb45fd01462164ee1897d3e62e
Gal1leox <79816710+Gal1leox@users.noreply.github.com>
2022-02-11 19:52:24 +1100

    restore to old

--- app.py
+++ app.py
@@ -6,13 +6,5 @@ app = Flask(__name__)
 def index ():
     return render_template('index.html')
 
-@app.route('home/')
-def index ():
-    return render_template('home.html')
-
-@app.route('leaderboard/')
-def index ():
-    return render_template('leaderboard.html')
-
 if __name__ ==  "__main__":
     app.run(debug=True)
