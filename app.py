e160180b16a981f80a7f7ba3968953bc1f68ec0f
Gal1leox <79816710+Gal1leox@users.noreply.github.com>
2022-02-11 20:00:27 +1100

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ def index ():
 
 if __name__ ==  "__main__":
     app.run(debug=True)
+
+#@app.route('/home')
+#def index ():
+#    return render_template('home.html')
+
+#@app.route('/leaderboard')
+#def index ():
+#    return render_template('leaderboard.html')
