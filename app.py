bc74959e7f916f23ec750e5d6a7dd56647280fbb
Gal1leox <79816710+Gal1leox@users.noreply.github.com>
2022-02-13 12:18:19 +1100

    dbsql foundations

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, url_for, request, redirect
+from flask_mysqldb import MySQL
+#import yaml
 
 app = Flask(__name__)
+
+#SQL to be added
+#db = yaml.load(open('db.yaml'))
+#app.config['MYSQL_HOST'] = db['mysql_host']
+#app.config['MYSQL_USER'] = db['mysql_user']
+#app.config['MYSQL_PASSWORD'] = db['mysql_password']
+#app.config['MYSQL_DB'] = db['mysql_db']
+
 
 @app.route('/')
 def index ():
