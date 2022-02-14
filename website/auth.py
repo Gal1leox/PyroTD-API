0c7144a2c52ce9b53382f7524d99fd7703ba0f56
Gal1leox <79816710+Gal1leox@users.noreply.github.com>
2022-02-15 09:44:30 +1100

    designing user model and db connection

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,20 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, url_for, request
 
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET','POST'])
 def login():
+    email = data = request.form.get("email")
+    password = data = request.form.get("password")
     return render_template ("Login.html")
 
-@auth.route("/sign_up")
+@auth.route("/sign_up", methods=['GET','POST'])
 def sign_up():
+    username = data = request.form.get("username")
+    email = data = request.form.get("email")
+    password1 = data = request.form.get("password1")
+    password2 = data = request.form.get("password2")
     return render_template ("sign_up.html")
 
 @auth.route("/logout")
