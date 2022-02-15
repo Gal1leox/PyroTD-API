e3e384fea9135d85dad497c2b2bacd45e68c4a8b
Gal1leox <79816710+Gal1leox@users.noreply.github.com>
2022-02-15 14:26:39 +1100

    user login features done

--- website/models.py
+++ website/models.py
@@ -7,4 +7,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    date_created = db.Column(db.DateTime(timezone=True), default=func.now)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
