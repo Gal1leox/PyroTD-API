4d7a577b96b6a41e1bccca6829c388f670fd2226
Gal1leox <79816710+Gal1leox@users.noreply.github.com>
2022-02-15 17:24:01 +1100

    added match model

--- website/models.py
+++ website/models.py
@@ -8,3 +8,15 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    winner = db.Column(db.String(150))
+    p1 = db.Column(db.String(150))
+    p2 = db.Column(db.String(150))
+    p3 = db.Column(db.String(150))
+    p4 = db.Column(db.String(150))
+    p5 = db.Column(db.String(150))
+    p6 = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
